33b69cfbdae138c3fb98c9c33d25d510d66f97b0
Filippo Fadda <filippo.fadda@gmail.com>
2015-02-09 23:54:10 +0100

    command to compact a database

---
+++ src/ElephantOnCouch/Console/Command/CompactCommand.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * @file CompactCommand.php
+ * @brief This file contains the CompactCommand class.
+ * @details
+ * @author Filippo F. Fadda
+ */
+
+
+namespace ElephantOnCouch\Console\Command;
+
+
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+use ElephantOnCouch\Couch;
+
+
+/**
+ * @brief Starts a compaction for the current selected database.
+ * @nosubgrouping
+ */
+class CompactCommand extends AbstractCommand {
+
+
+  /**
+   * @brief Configures the command.
+   */
+  protected function configure() {
+    $this->setName("compact");
+    $this->setDescription("Starts a compaction for the current selected database.");
+  }
+
+
+  /**
+   * @brief Executes the command.
+   */
+  protected function execute(InputInterface $input, OutputInterface $output) {
+    $couch = $this->di['couchdb'];
+
+    $couch->compactDb();
+
+    parent::execute($input, $output);
+  }
+
+}
