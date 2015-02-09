50963b20dce4a4ac97b734da5a1358a85a2c08dc
Filippo Fadda <filippo.fadda@gmail.com>
2015-02-09 23:54:34 +0100

    command to cleanup a view

---
+++ src/ElephantOnCouch/Console/Command/CleanupCommand.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * @file CleanupCommand.php
+ * @brief This file contains the CleanupCommand class.
+ * @details
+ * @author Filippo F. Fadda
+ */
+
+
+namespace ElephantOnCouch\Console\Command;
+
+
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+
+/**
+ * @brief Removes all outdated view indexes.
+ * @nosubgrouping
+ */
+class CleanupCommand extends AbstractCommand {
+
+
+  /**
+   * @brief Configures the command.
+   */
+  protected function configure() {
+    $this->setName("cleanup");
+    $this->setDescription("Removes all outdated view indexes.");
+  }
+
+
+  /**
+   * @brief Executes the command.
+   */
+  protected function execute(InputInterface $input, OutputInterface $output) {
+    $couch = $this->di['couchdb'];
+
+    $couch->cleanupViews();
+
+    parent::execute($input, $output);
+  }
+
+}
