ed2713c636a080d383c0bd7e44b2934538f1f8d2
Filippo Fadda <filippo.fadda@gmail.com>
2015-02-09 23:55:36 +0100

    the main console class

---
+++ src/ElephantOnCouch/Console/Console.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * @file Console.php
+ * @brief This file contains the Console class.
+ * @details
+ * @author Filippo F. Fadda
+ */
+
+
+/**
+ * @brief This namespace contains the Console class.
+ */
+namespace ElephantOnCouch\Console;
+
+
+use Symfony\Component\Console\Application;
+
+
+/**
+ * @brief This class extends the Application class of Symfony framework, with methods aim to set the Phalcon Dependency
+ * Injector.
+ */
+class Console extends Application {
+
+  protected $_di;
+
+
+
+}
