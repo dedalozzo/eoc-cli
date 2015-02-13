c23ee5a46e69cb0a74ff00d4122aeb5b329d7c23
Filippo Fadda <filippo.fadda@gmail.com>
2015-02-13 02:37:09 +0100

    modified the commands list

--- bin/couch.php
+++ bin/couch.php
@@ -36,41 +36,32 @@ try {
   ErrorHandler::register($monolog);
 
   // Creates a stream handler to log debugging messages.
-  $monolog->pushHandler(new StreamHandler($root.$config->application->logDir."pit.log", Logger::DEBUG));
+  $monolog->pushHandler(new StreamHandler($root.$config->application->logDir."couch.log", Logger::DEBUG));
 
   // Creates the application object.
-  $console = new Console('PitPress Console', Version::getNumber());
+  $console = new Console('ElephantOnCouch Console', Version::getNumber());
   $console->setCatchExceptions(FALSE);
 
-  $console->add(new Command\AboutCommand());
+  $console->add(new Command\VersionCommand());
+  $console->add(new Command\StatusCommand());
   $console->add(new Command\UseCommand());
-  $console->add(new Command\CleanupCommand());
+  $console->add(new Command\InfoCommand());
   $console->add(new Command\CommitCommand());
-  $console->add(new Command\CompactCommand());
+  $console->add(new Command\CompactCommand()); //compactdb and compactview parameter --view=
+  $console->add(new Command\CleanupCommand());
   $console->add(new Command\CreateCommand());
   $console->add(new Command\DeleteCommand());
   $console->add(new Command\QueryCommand());
-  $console->add(new Command\StatusCommand());
+  $console->add(new Command\RestartCommand());
+  $console->add(new Command\LogCommand());
 
-  //  $console->add(new Command\RestartServerCommand());
-  //  $console->add(new Command\ServerInfoCommand());
-  //  $console->add(new Command\ClientInfoCommand());
   //  $console->add(new Command\Stats());
   //  $console->add(new Command\AllDbs());
   //  $console->add(new Command\DbUpdates());
-  //  $console->add(new Command\ActiveTasks());
-  //  $console->add(new Command\LogTail());
-  //  $console->add(new Command\Uuids());
   //  $console->add(new Command\getConfig());
   //  $console->add(new Command\setKey());
   //  $console->add(new Command\deleteKey());
-  //  $console->add(new Command\SelectDbCommand());
-  //  $console->add(new Command\CreateDbCommand());
-  //  $console->add(new Command\DeleteDbCommand());
-  //  $console->add(new Command\DbInfoCommand());
   //  $console->add(new Command\DbChangesCommand());
-  //  $console->add(new Command\CompactDbCommand());
-  //  $console->add(new Command\CompactViewCommand());
   //  $console->add(new Command\StartReplicationCommand());
   //  $console->add(new Command\StopReplicationCommand());
 
