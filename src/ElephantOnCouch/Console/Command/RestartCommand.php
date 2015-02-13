7edfe783ca3bcda4e641df498b89a8ab623ba8c4
Filippo Fadda <filippo.fadda@gmail.com>
2015-02-13 02:32:34 +0100

    added the restart command

---
+++ src/ElephantOnCouch/Console/Command/RestartCommand.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * @file RestartCommand.php
+ * @brief This file contains the RestartCommand class.
+ * @details
+ * @author Filippo F. Fadda
+ */
+
+
+namespace ElephantOnCouch\Console\Command;
+
+
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+
+/**
+ * @brief Restarts CouchDB.
+ * @nosubgrouping
+ */
+class RestartCommand extends AbstractCommand {
+
+
+  /**
+   * @brief Configures the command.
+   */
+  protected function configure() {
+    $this->setName("restart");
+    $this->setDescription("Restarts CouchDB server.");
+  }
+
+
+  /**
+   * @brief Executes the command.
+   * @param[in] InputInterface $input The input interface
+   * @param[in] OutputInterface $output The output interface
+   * @return string Information about CouchDB's client, server and the PitPress database.
+   */
+  protected function execute(InputInterface $input, OutputInterface $output) {
+    $this->couch->restart();
+
+    parent::execute($input, $output);
+  }
+
+}
