d12c2bfba9989db948e2a3b785ed7fcd40aa11cd
Filippo Fadda <filippo.fadda@gmail.com>
2015-02-13 02:32:46 +0100

    added the log command

---
+++ src/ElephantOnCouch/Console/Command/LogCommand.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * @file LogCommand.php
+ * @brief This file contains the LogCommand class.
+ * @details
+ * @author Filippo F. Fadda
+ */
+
+
+namespace ElephantOnCouch\Console\Command;
+
+
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+
+/**
+ * @brief Returns the tail of the server's log file.
+ * @nosubgrouping
+ */
+class LogCommand extends AbstractCommand {
+
+
+  /**
+   * @brief Configures the command.
+   */
+  protected function configure() {
+    $this->setName("log");
+    $this->setDescription("Returns the tail of the server's log file.");
+
+    // General options.
+    $this->addOption("bytes",
+      NULL,
+      InputOption::VALUE_OPTIONAL,
+      "How many bytes to return from the end of the log file.",
+      1000);
+  }
+
+
+  /**
+   * @brief Executes the command.
+   */
+  protected function execute(InputInterface $input, OutputInterface $output) {
+    $bytes = (int)$input->getOption('bytes');
+    $this->couch->getLogTail($bytes);
+
+    parent::execute($input, $output);
+  }
+
+}
