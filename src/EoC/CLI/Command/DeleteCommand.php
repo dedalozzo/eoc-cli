27dce389a8b7de5f927a14244ca776aac788607a
Filippo Fadda <filippo.fadda@gmail.com>
2015-02-26 15:50:58 +0100

    modified DeleteCommand description

--- src/EoC/CLI/Command/DeleteCommand.php
+++ src/EoC/CLI/Command/DeleteCommand.php
@@ -28,7 +28,7 @@ class DeleteCommand extends AbstractCommand {
    */
   protected function configure() {
     $this->setName("delete");
-    $this->setDescription("Deletes the PitPress database.");
+    $this->setDescription("Deletes the specified database, if not in use.");
 
     $this->addArgument("database",
       InputArgument::REQUIRED,
