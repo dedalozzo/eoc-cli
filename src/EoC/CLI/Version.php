96959816c1330690f94e1c6570d14b1eab71309e
Filippo Fadda <filippo.fadda@gmail.com>
2015-02-26 19:31:05 +0100

    updated MINOR to 3

--- src/EoC/CLI/Version.php
+++ src/EoC/CLI/Version.php
@@ -15,6 +15,6 @@ namespace EoC\CLI;
 class Version extends \EoC\Version {
 
   const MAJOR = '0'; //!< Major release number.
-  const MINOR = '2'; //!< Minor release number.
+  const MINOR = '3'; //!< Minor release number.
   const MAINTENANCE = '0'; //!< Maintenance release number (bug fixes only).
 } 
