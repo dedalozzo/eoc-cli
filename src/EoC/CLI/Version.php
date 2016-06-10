d54d1cd10098a3b88de51edb41750d4cd5f7bcb0
Filippo Fadda <filippo.fadda@gmail.com>
2016-06-10 15:49:09 +0200

    updated version

--- src/EoC/CLI/Version.php
+++ src/EoC/CLI/Version.php
@@ -18,5 +18,5 @@ class Version extends \EoC\Version {
 
   const MAJOR = '0'; //!< Major release number.
   const MINOR = '4'; //!< Minor release number.
-  const MAINTENANCE = '2'; //!< Maintenance release number (bug fixes only).
+  const MAINTENANCE = '3'; //!< Maintenance release number (bug fixes only).
 } 
